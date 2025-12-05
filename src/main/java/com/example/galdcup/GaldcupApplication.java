b19ca50f4bd987476e3716b77409e021281d2b28
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-05 19:41:37 +0900

    Initial commit

---
+++ src/main/java/com/example/galdcup/GaldcupApplication.java
@@ -0,0 +1,13 @@
+package com.example.galdcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaldcupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaldcupApplication.class, args);
+	}
+
+}
