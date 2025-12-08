0db43c815f236f2ab848a9d8722b4af17033ce95
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-09 03:29:09 +0900

    feat: 사용자 조회 메서드 추가(OauthId, Email, Nickname)

--- src/main/java/com/example/galdcup/repository/UserRepository.java
+++ src/main/java/com/example/galdcup/repository/UserRepository.java
@@ -3,5 +3,12 @@ package com.example.galdcup.repository;
 import com.example.galdcup.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByOauthId(String oauthId);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
 }
