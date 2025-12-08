998ea9f88eb6d86a2816a4cbe16a5273557abbfb
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-09 03:22:44 +0900

    feat: Spring security 설정 생성(Oauth 관련)

---
+++ src/main/java/com/example/galdcup/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.galdcup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/me").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth -> oauth.defaultSuccessUrl("/"))
+                .sessionManagement(session -> session.sessionFixation().migrateSession());
+
+        return http.build();
+    }
+}
