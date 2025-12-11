c383c9c6c0abd1e280c72e95c18fd0ce91e3ed5e
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-11 18:04:24 +0900

    feat: Post 관련 조회 메서드 추가

--- src/main/java/com/example/galdcup/repository/PostRepository.java
+++ src/main/java/com/example/galdcup/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.example.galdcup.repository;
 import com.example.galdcup.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByBoardId(Long boardId);
+    List<Post> findByAuthorNickname(String nickname);
 }
