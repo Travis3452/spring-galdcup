d4c60539f383c5a51b65fd7d76afad19600a15c0
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-12 03:23:48 +0900

    refactor: 작성자의 변수 네이밍 수정

--- src/main/java/com/example/galdcup/entity/Comment.java
+++ src/main/java/com/example/galdcup/entity/Comment.java
@@ -3,6 +3,7 @@ package com.example.galdcup.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,30 +14,32 @@ public class Comment {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 어떤 Post에 속하는지
+    // 댓글이 작성된 게시글
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
     // 작성자
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "created_by", nullable = false)
-    private User createdBy;
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
 
     // 댓글 내용
     @Column(nullable = false, length = 300)
     private String content;
 
-    // 대댓글들
+    // 대댓글
     @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Reply> replies;
+    private List<Reply> replies = new ArrayList<>();
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     @PrePersist
     public void prePersist() {
-        createdAt = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
     }
 
     @PreUpdate
