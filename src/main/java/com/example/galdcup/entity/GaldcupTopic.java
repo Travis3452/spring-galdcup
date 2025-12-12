866116b5f17317e69afe5d1d0bf33424afb5c87b
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-13 05:21:24 +0900

    refactor: GaldcupResult와의 관계 삭제

--- src/main/java/com/example/galdcup/entity/GaldcupTopic.java
+++ src/main/java/com/example/galdcup/entity/GaldcupTopic.java
@@ -25,14 +25,10 @@ public class GaldcupTopic {
     private LocalDateTime endTime;
 
     // 토론 게시판 (Board와 1:1 관계)
-    @OneToOne(mappedBy = "topic", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "galdcupTopic", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Board board;
 
     // 투표 세션 (투표 관리 전담 엔티티)
-    @OneToOne(mappedBy = "topic", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "galdcupTopic", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private VoteSession voteSession;
-
-    // 결과 (투표 종료 후 집계)
-    @OneToOne(mappedBy = "topic", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private GaldcupResult result;
 }
