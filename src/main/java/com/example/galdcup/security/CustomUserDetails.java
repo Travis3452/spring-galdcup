96005bbc5263accb5826e2cc8e88a9997eeaed58
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-19 08:38:14 +0900

    refactor: Getter 어노테이션 추가

--- src/main/java/com/example/galdcup/security/CustomUserDetails.java
+++ src/main/java/com/example/galdcup/security/CustomUserDetails.java
@@ -1,22 +1,20 @@
 package com.example.galdcup.security;
 
 import com.example.galdcup.entity.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 public class CustomUserDetails implements UserDetails {
 
     private final User user;
 
     public CustomUserDetails(User user) {
         this.user = user;
-    }
-
-    public User getUser() {
-        return user;
     }
 
     @Override
