270ab5ad8747c5c6a364374226a4b9ddbd7b055f
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-22 05:25:40 +0900

    refactor: Post 조회 로직에 Pagination 적용

--- src/main/java/com/example/galdcup/repository/PostRepository.java
+++ src/main/java/com/example/galdcup/repository/PostRepository.java
@@ -1,11 +1,14 @@
 package com.example.galdcup.repository;
 
 import com.example.galdcup.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByBoardId(Long boardId);
-    List<Post> findByAuthorNickname(String nickname);
+    Page<Post> findByBoardId(Long boardId, Pageable pageable);
+    Page<Post> findByAuthorNickname(String nickname, Pageable pageable);
+
 }
