129523f2f8351caec02102d6c7e69ac351ad4079
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-23 10:17:55 +0900

    refactor: 불필요한 email 변수 제거

--- src/main/java/com/example/galdcup/dto/user/UpdateUserRequest.java
+++ src/main/java/com/example/galdcup/dto/user/UpdateUserRequest.java
@@ -1,12 +1,8 @@
 package com.example.galdcup.dto.user;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
 public record UpdateUserRequest(
-        @Email @Size(max = 50)
-        String email,
-
         @Size(min = 2, max = 20)
         String nickname
 ) {}
