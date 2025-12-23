cbc370671716f9fecc84272c4cd81382526562a4
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-23 10:14:48 +0900

    refactor: 불필요한 변수 authorId 제거

--- src/main/java/com/example/galdcup/dto/post/CreatePostRequest.java
+++ src/main/java/com/example/galdcup/dto/post/CreatePostRequest.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Size;
 
 public record CreatePostRequest(
         @NotNull Long boardId,
-        @NotNull Long authorId,
         @Size(min = 1, max = 50) String title,
         @Size(min = 1, max = 2000) String content
 ) {}
