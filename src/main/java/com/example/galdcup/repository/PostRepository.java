6046b59f619fd8125724cc1132166a335d42ff9c
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-26 09:31:45 +0900

    refactor: 게시글 조회수 업데이트 쿼리 추가

--- src/main/java/com/example/galdcup/repository/PostRepository.java
+++ src/main/java/com/example/galdcup/repository/PostRepository.java
@@ -4,11 +4,15 @@ import com.example.galdcup.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByBoardId(Long boardId, Pageable pageable);
     Page<Post> findByAuthorNickname(String nickname, Pageable pageable);
 
+    @Modifying
+    @Query("UPDATE Post post SET post.view = post.view + :views WHERE post.id = :id")
+    void updateViewCount(@Param("id") Long id, @Param("views") long views);
 }
