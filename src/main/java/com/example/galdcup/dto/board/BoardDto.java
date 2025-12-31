7ce7f4f0942119229bfb81ef4e87775ed6c70216
Travis3452 <rnjswlsdn3452@gmail.com>
2025-12-31 21:58:16 +0900

    refactor: 게시판의 설명에 대한 컬럼 추가에 대한 변경사항 반영

--- src/main/java/com/example/galdcup/dto/board/BoardDto.java
+++ src/main/java/com/example/galdcup/dto/board/BoardDto.java
@@ -3,19 +3,22 @@ package com.example.galdcup.dto.board;
 import com.example.galdcup.entity.Board;
 import lombok.Builder;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Builder
 public record BoardDto(
         Long id,
         String topic,
+        String description,
         Board.Status status,
         LocalDateTime createdAt
-) {
+) implements Serializable {
     public static BoardDto from(Board board) {
         return BoardDto.builder()
                 .id(board.getId())
                 .topic(board.getTopic())
+                .description(board.getDescription())
                 .status(board.getStatus())
                 .createdAt(board.getCreatedAt())
                 .build();
