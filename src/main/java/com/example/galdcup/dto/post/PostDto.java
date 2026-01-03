d85305eea2e7e2cab623af416e287763c5874711
Travis3452 <rnjswlsdn3452@gmail.com>
2026-01-03 22:19:49 +0900

    fix: authorId(작성자 PK), view(조회수), likeCount(좋아요 수), dislikeCount(싫어요 수)를 포함하도록 변경

--- src/main/java/com/example/galdcup/dto/post/PostDto.java
+++ src/main/java/com/example/galdcup/dto/post/PostDto.java
@@ -9,18 +9,26 @@ import java.time.LocalDateTime;
 public record PostDto(
         Long id,
         Long boardId,
+        Long authorId,
         String authorNickname,
+        Long view,
         String title,
         String content,
+        Long likeCount,
+        Long dislikeCount,
         LocalDateTime createdAt
 ) {
     public static PostDto from(Post post) {
         return PostDto.builder()
                 .id(post.getId())
                 .boardId(post.getBoard().getId())
+                .authorId(post.getAuthor().getId())
                 .authorNickname(post.getAuthor().getNickname())
+                .view(post.getView())
                 .title(post.getTitle())
                 .content(post.getContent())
+                .likeCount(post.getLikeCount())
+                .dislikeCount(post.getDislikeCount())
                 .createdAt(post.getCreatedAt())
                 .build();
     }
