d61b9d9a52aae50cb4fde8850bed350e026afa31
Travis3452 <rnjswlsdn3452@gmail.com>
2026-01-03 22:17:47 +0900

    refactor: 스케줄러 활성화를 위해 @EnableScheduling 어노테이션 추가

--- src/main/java/com/example/galdcup/GaldcupApplication.java
+++ src/main/java/com/example/galdcup/GaldcupApplication.java
@@ -2,12 +2,12 @@ package com.example.galdcup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GaldcupApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GaldcupApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GaldcupApplication.class, args);
+    }
 }
