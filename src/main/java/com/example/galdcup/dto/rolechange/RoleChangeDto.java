f82643e04852c4be4aba1b970c3b32fc17848077
Travis3452 <rnjswlsdn3452@gmail.com>
2026-01-07 19:39:09 +0900

    refactor: Dto에 권한 요청 사용자의 email도 포함하도록 변경

--- src/main/java/com/example/galdcup/dto/rolechange/RoleChangeDto.java
+++ src/main/java/com/example/galdcup/dto/rolechange/RoleChangeDto.java
@@ -6,14 +6,16 @@ import com.example.galdcup.entity.User;
 public record RoleChangeDto(
         Long id,
         Long userId,
+        String email,
         String nickname,
         User.Role requestedRole,
         RoleChange.Status status
 ) {
-    public static RoleChangeDto from(RoleChange roleChange) {
+    public static RoleChangeDto from(RoleChange roleChange, String emailDecrypted) {
         return new RoleChangeDto(
                 roleChange.getId(),
                 roleChange.getUser().getId(),
+                emailDecrypted,
                 roleChange.getUser().getNickname(),
                 roleChange.getRequestedRole(),
                 roleChange.getStatus()
