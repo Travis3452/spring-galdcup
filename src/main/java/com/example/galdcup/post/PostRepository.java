2033733acbff999a6b9fc9b02eb7b39eea5d4407
Travis3452 <rnjswlsdn3452@gmail.com>
2026-01-10 00:47:01 +0900

    refactor: 더 이상 불필요한 메서드 제거

--- src/main/java/com/example/galdcup/post/PostRepository.java
+++ src/main/java/com/example/galdcup/post/PostRepository.java
@@ -1,6 +1,5 @@
 package com.example.galdcup.post;
 
-import com.example.galdcup.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,8 +14,4 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Modifying
     @Query("UPDATE Post post SET post.view = post.view + :views WHERE post.id = :id")
     void updateViewCount(@Param("id") Long id, @Param("views") long views);
-
-    @Modifying
-    @Query("update Post p set p.author = :deletedUser where p.author.id = :userId")
-    void updateAuthorToDeletedUser(@Param("userId") Long userId, @Param("deletedUser") User deletedUser);
 }
