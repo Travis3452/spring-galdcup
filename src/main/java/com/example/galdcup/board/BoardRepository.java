acb7442287116bb4736051a6f139c9877bd14176
Travis3452 <rnjswlsdn3452@gmail.com>
2026-01-10 01:42:06 +0900

    fix: 회원 탈퇴 시 User가 FK로 사용되고 있는 board의 boardManager을 제거하도록 수정

--- src/main/java/com/example/galdcup/board/BoardRepository.java
+++ src/main/java/com/example/galdcup/board/BoardRepository.java
@@ -1,6 +1,13 @@
 package com.example.galdcup.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Modifying
+    @Query("UPDATE Board b SET b.boardManager = NULL WHERE b.boardManager.id = :userId")
+    void removeBoardManagerByUserId(@Param("userId") Long userId);
 }
