64034f7c256ed9aa6ccc7ad8ed92f01a9a670f4d
Travis3452 <rnjswlsdn3452@gmail.com>
2026-01-11 22:50:15 +0900

    feat: Dto에 투표 수 데이터를 포함하도록 변경

--- src/main/java/com/example/galdcup/vote/dto/VoteOptionDto.java
+++ src/main/java/com/example/galdcup/vote/dto/VoteOptionDto.java
@@ -1,15 +1,17 @@
 package com.example.galdcup.vote.dto;
 
-import com.example.galdcup.vote.embedded.VoteOption;
+import com.example.galdcup.vote.VoteOption;
 
 public record VoteOptionDto(
         String label,
-        String imageUrl
+        String imageUrl,
+        Long count
 ) {
     public static VoteOptionDto from(VoteOption option) {
         return new VoteOptionDto(
                 option.getLabel(),
-                option.getImageUrl()
+                option.getImageUrl(),
+                option.getCount()
         );
     }
 }
