f3e4861a563c41f477358286c6b3b130755616b5
Travis3452 <rnjswlsdn3452@gmail.com>
2026-01-11 22:54:41 +0900

    feat: VoteSession 종료 스케쥴링에 필요한 조회 메서드 추가

--- src/main/java/com/example/galdcup/vote/VoteSessionRepository.java
+++ src/main/java/com/example/galdcup/vote/VoteSessionRepository.java
@@ -2,5 +2,10 @@ package com.example.galdcup.vote;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 public interface VoteSessionRepository extends JpaRepository<VoteSession, Long> {
+
+    List<VoteSession> findByEndTimeBeforeAndIsFinishedFalse(OffsetDateTime now);
 }
