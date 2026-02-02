333c6fa9a087186329447da9e1b38c4f0c2992a0
Travis3452 <rnjswlsdn3452@gmail.com>
2026-02-02 21:18:07 +0900

    refactor: RefreshToken을 더 이상 mysql에 저장하지 않고 redis에 저장하도록 개선

--- src/main/java/com/example/galdcup/auth/RefreshToken.java
+++ src/main/java/com/example/galdcup/auth/RefreshToken.java
@@ -1,27 +1,20 @@
 package com.example.galdcup.auth;
 
-import com.example.galdcup.user.User;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import java.time.OffsetDateTime;
 
-@Entity
-@Table(name = "refresh_tokens",
-        indexes = @Index(name = "idx_user_token", columnList = "user_id, token", unique = true))
+@RedisHash(value = "refreshToken", timeToLive = 604800)
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class RefreshToken {
+    @Id
+    private Long userId;
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false, unique = true)
-    private User user;
-
-    @Column(nullable = false, length = 500)
+    @Indexed
     private String token;
 
-    @Column(nullable = false)
     private OffsetDateTime expiryDate;
 }
